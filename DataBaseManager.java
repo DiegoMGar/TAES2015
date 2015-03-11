c7fb970dbb55dec742e1825fbef187f0ddc9aa02
Diego Maroto <diego.diemg@gmail.com>
2015-03-11 18:18:22 +0100

    Update DataBaseManager.java

--- DataBaseManager.java
+++ DataBaseManager.java
@@ -5,13 +5,12 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
-/**
- * @author Diego Maroto Garcia
- * @version 11.03.2015
- */
 public class DataBaseManager {
+
     private DbHelper helper;
     private SQLiteDatabase db;
+
+    /* CONSTRUCTOR Y CONEXION A LA BASE DE DATOS **************************************************/
     public DataBaseManager(Context context){
         helper = new DbHelper(context);
     }
@@ -21,6 +20,53 @@ public class DataBaseManager {
     public void openReadable(){
         db = helper.getReadableDatabase();
     }
+
+    /* FUNCIONES CRUD, INSERTAR,LEER,MODIFICAR Y ELIMINAR EN DB ***********************************/
+    public Cursor readAll(String table,String selects){
+        String[] select = selects.split(",");
+        return db.query(table,select,null,null,null,null,null);
+    }
+    public Cursor readForOID(String table,String selects,Integer id){
+        String[] select = selects.split(",");
+        return db.query(table,select,ALL_ID+"=?",new String[]{id.toString()},null,null,null);
+    }
+    public boolean insert(String tabla,String mapaTextual) {
+        if (mapaTextual.length() == 0)
+            return false;
+        long resultado = db.insert(tabla, null, generateContentValues(mapaTextual));
+        return resultado > -1;
+    }
+    public boolean updateForOID(String tabla,String mapaTextual,Integer id){
+        int resultado = db.update(tabla,generateContentValues(mapaTextual),
+                                    ALL_ID+"=?",new String[]{id.toString()});
+        return resultado>0; // Si zero, no ha afectado a ninguna columna
+    }
+    public boolean updateALL(String tabla,String mapaTextual){
+        int resultado = db.update(tabla,generateContentValues(mapaTextual),null,null);
+        return resultado>0; // Si zero, no ha afectado a ninguna columna
+    }
+    public boolean deleteForOID(String tabla,Integer id){
+        int resultado = db.delete(tabla,ALL_ID+"=?",new String[]{id.toString()});
+        return resultado>0; // Si zero, no ha eliminado nada
+    }
+    public boolean deleteALL(String tabla){
+        int resultado = db.delete(tabla,"1",null);
+        return resultado>0; // Si zero, no ha eliminado nada
+    }
+    private ContentValues generateContentValues(String mapaTextual){
+        //"key1-value1,key2-value2,key3-value3..."
+        if(mapaTextual==null || mapaTextual.length()==0)
+            return null;
+        ContentValues valores = new ContentValues();
+        String[] filas = mapaTextual.split(",");
+        for(String fila : filas){
+            String[] llave_valor = fila.split("-");
+            valores.put(llave_valor[0],llave_valor[1]);
+        }
+        return valores;
+    }
+
+    /* PARAMETROS STATIC, NOMBRES DE TABLAS Y COLUMNAS DE TABLAS **********************************/
     public static final String TABLE_TEAM = "equipos";
     public static final String TABLE_PLAYERS = "jugadores";
     public static final String TABLE_GAMES = "partidos";
@@ -60,48 +106,6 @@ public class DataBaseManager {
             "foreign key("+TG_equipo+") references "+TABLE_TEAM+"("+ALL_ID+")" +
             ");";
 
-    public Cursor readAll(String table,String selects){
-        String[] select = selects.split(",");
-        return db.query(table,select,null,null,null,null,null);
-    }
-    public Cursor readForOID(String table,String selects,Integer id){
-        String[] select = selects.split(",");
-        return db.query(table,select,ALL_ID+"=?",new String[]{id.toString()},null,null,null);
-    }
-    public boolean insert(String tabla,String mapaTextual) {
-        if (mapaTextual.length() == 0)
-            return false;
-        long resultado = db.insert(tabla, null, generateContentValues(mapaTextual));
-        return resultado > -1;
-    }
-    public boolean updateForOID(String tabla,String mapaTextual,Integer id){
-        int resultado = db.update(tabla,generateContentValues(mapaTextual),ALL_ID+"=?",new String[]{id.toString()});
-        return resultado>0; // Si zero, no ha afectado a ninguna columna
-    }
-    public boolean updateALL(String tabla,String mapaTextual){
-        int resultado = db.update(tabla,generateContentValues(mapaTextual),null,null);
-        return resultado>0; // Si zero, no ha afectado a ninguna columna
-    }
-    public boolean deleteForOID(String tabla,Integer id){
-        int resultado = db.delete(tabla,ALL_ID+"=?",new String[]{id.toString()});
-        return resultado>0; // Si zero, no ha eliminado nada
-    }
-    public boolean deleteALL(String tabla){
-        int resultado = db.delete(tabla,"1",null);
-        return resultado>0; // Si zero, no ha eliminado nada
-    }
-    private ContentValues generateContentValues(String mapaTextual){
-        //"key1-value1,key2-value2,key3-value3..."
-        if(mapaTextual==null || mapaTextual.length()==0)
-            return null;
-        ContentValues valores = new ContentValues();
-        String[] filas = mapaTextual.split(",");
-        for(String fila : filas){
-            String[] llave_valor = fila.split("-");
-            valores.put(llave_valor[0],llave_valor[1]);
-        }
-        return valores;
-    }
-    /* FALTA VER DE SOLUCIONAR EL ANR TRABAJANDO EN SEGUNDO PLANO */
+    /* FALTA VER DE SOLUCIONAR EL ANR TRABAJANDO EN SEGUNDO PLANO *********************************/
 
 }
